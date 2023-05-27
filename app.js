72c4a7a6b170a5a9ec89e9941a9f242bf52dbc6c
dipakpratale158 <112299138+dipakpratale158@users.noreply.github.com>
2023-05-27 13:55:08 +0530

    inital express live server

--- app.js
+++ app.js
@@ -1,30 +1,8 @@
-
-///node js non blocking code asynchronous behaiver
-
-const http=require('http')
-const routes=require('./router')
-const server=http.createServer(routes.routes)
-  console.log(routes.text)
+const express=require('express')
 
 
-server.listen(3001,()=>{
-    console.log('server listening at 3001 port')
+const app=express()
+
+app.listen(3001,()=>{
+    console.log("server live 3000")
 })
-
-
-//npm init 
-//pacage name 
-//version
-// description
-//entry point
-//test command
-//git repository:
-//keyword
-//author:enter name autor
-// licen
-//enter
-//yes
-// go check vs code pacage.json file created
-//npm install nodemon --save-dev   not run again and agin save automaticaly
-//npm start
-// go pacage json enter start:node app.js insted of nodemon app.js
