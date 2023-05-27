cfb1b3494647a792210ee1b425196af0e145d82b
dipakpratale158 <112299138+dipakpratale158@users.noreply.github.com>
2023-05-27 14:37:08 +0530

    how to solve problem matcing

--- app.js
+++ app.js
@@ -14,16 +14,47 @@ const app=express()
     //when see console only first middelwher execute
 
 
-    app.use((req,res,next)=>{
-        console.log("first middleware")
-        next()
-        })
+    // app.use((req,res,next)=>{
+    //     console.log("first middleware")
+    //     next()
+    //     })
         
-        app.use((req,res,next)=>{
-            console.log("second middleware")
-            })
+    //     app.use((req,res,next)=>{
+    //         console.log("second middleware")
+    //         })
 
-    //when see console using next method  first middelwher and second middleware  execute
+    // //when see console using next method  first middelwher and second middleware  execute
+
+
+//request routing
+// app.use("/",(req,res,next)=>{
+//     console.log("first request")
+// })
+
+
+
+//sending request appropriate
+// browser display content
+// app.use("/",(req,res,next)=>{
+// // res.send("this is the home page")
+// //convert content type to application/json
+// res.send({name:"dipak"})
+// })
+
+app.use((req,res,next)=>{
+    console.log('login the request')
+    //if i am next not move next because already set response
+
+})
+
+app.use('/',(req,res,next)=>{
+    res.send('<h1>this is user page dipak</h1>')
+})
+
+//  / matchest so go above this code 
+app.use('/users',(req,res,next)=>{
+    res.send('this is user page')
+})
 
 
 //when open server not send any requset so  not showing anything in server or google 
