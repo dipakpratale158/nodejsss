c43c0aac369b34b420be5aaefae5c70629c182c4
dipakpratale158 <112299138+dipakpratale158@users.noreply.github.com>
2023-05-27 14:07:32 +0530

    How Middleware works in Express JS and add Middleware in the Node Project - NodeJ

--- app.js
+++ app.js
@@ -2,6 +2,31 @@ const express=require('express')
 
 
 const app=express()
+//whenever incoming message 
+// app.use((req,res,next)=>{
+// console.log("first middleware")
+// })
+
+// app.use((req,res,next)=>{
+//     console.log("second middleware")
+//     })
+
+    //when see console only first middelwher execute
+
+
+    app.use((req,res,next)=>{
+        console.log("first middleware")
+        next()
+        })
+        
+        app.use((req,res,next)=>{
+            console.log("second middleware")
+            })
+
+    //when see console using next method  first middelwher and second middleware  execute
+
+
+//when open server not send any requset so  not showing anything in server or google 
 
 app.listen(3001,()=>{
     console.log("server live 3000")
